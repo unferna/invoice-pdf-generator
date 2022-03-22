9ff28e4864a920d2b9df9c715044f51b10b314c1
Fernando Florez <fernando.florez@mattersupply.co>
2022-03-21 20:46:36 -0500

    [Feature] HTML Template parsed to class

--- HTMLSource/index.js
+++ HTMLSource/index.js
@@ -1,89 +1,116 @@
-const invoiceData = require("../default-data.json")
 const styles = require("./HtmlReportStyles")
 
-const Utils = require("../Utils")
+class PDFReport {
+    invoiceData = {}
+    totalInvoice = 0
+    invoiceDateString = ""
 
-const now = new Date()
-const invoiceDateString = [
-    Utils.addLeftZeroIfNeeded( now.getDate() ),
-    Utils.addLeftZeroIfNeeded( now.getMonth() + 1 ),
-    now.getFullYear()
-].join("/")
+    constructor() {
+        this.invoiceData = {}
+        this.totalInvoice = 0
+        this.invoiceDateString = ""
+    }
 
-const totalInvoice = invoiceData.itemsToCharge
-    .map(item => item.total)
-    .reduce((prev, current) => prev + current, 0)
+    loadInvoiceData(data) {
+        this.invoiceData = data
+        this.totalInvoice = data.itemsToCharge
+            .map(item => item.total)
+            .reduce((prev, current) => prev + current, 0)
+    }
 
-const TermsAndCondtionsComponent = (totalInvoice, termsAndConditionsData) => {
-    return `<div class="invoice-terms-and-conditions">
-            <h3>TERMS & CONDITIONS</h3>
+    TermsAndCondtionsComponent() {
+        const { termsAndConditionsData } = this.invoiceData
+        const bank = termsAndConditionsData.find(item => {
+            return item.key === "bank"
+        }).value
 
-            <p>Please transfer:<br /><br />$${totalInvoice} to ${ termsAndConditionsData.bank } Account ${ termsAndConditionsData.account }<br />Routing Number: ${ termsAndConditionsData.routingNumber }<br />Bank Branch Address:<br />${ termsAndConditionsData.bankCity }<br />${ termsAndConditionsData.bankAddress }</p>
-        </div>`
-}
+        const account = termsAndConditionsData.find(item => {
+            return item.key === "bank"
+        }).value
 
-const FullReport = () => {
-    return `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <title>03 - March Invoice</title>
-            <style>
-            ${ styles }
-            </style>
-        </head>
-    <body>
-        <div class="html-report-container">
-            <div class="invoice-date-container">
-                <p>Invoice Date: ${ invoiceDateString }</p>
-            </div>
+        const routingNumber = termsAndConditionsData.find(item => {
+            return item.key === "routingNumber"
+        }).value
 
-            <ul class="invoice-heading-data-list">
-                <li><strong>${ invoiceData.fullName }</strong></li>
-                ${ invoiceData.headingData.map(data => (
-                    `<li>${ data.label }: ${ data.value }</li>`
-                )).join("") }
-            </ul>
+        const bankCity = termsAndConditionsData.find(item => {
+            return item.key === "bankCity"
+        }).value
 
-            <table class="invoice-items-table">
-                <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    ${ invoiceData.itemsToCharge.map(item => (
-                        `<tr>
-                            <td>${ item.description }</td>
-                            <td>${ invoiceData.coinToUse } ${ item.total }</td>
-                        </tr>`
+        const bankAddress = termsAndConditionsData.find(item => {
+            return item.key === "bankAddress"
+        }).value
+
+        const resultString = `<div class="invoice-terms-and-conditions">
+                <h3>TERMS & CONDITIONS</h3>
+    
+                <p>Please transfer:<br /><br />$${ this.totalInvoice } to ${ bank } Account ${ account }<br />Routing Number: ${ routingNumber }<br />Bank Branch Address:<br />${ bankCity }<br />${ bankAddress }</p>
+            </div>`
+
+        return resultString
+    }
+
+    reportHTMLString() {
+        return `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>03 - March Invoice</title>
+                <style>
+                ${ styles }
+                </style>
+            </head>
+        <body>
+            <div class="html-report-container">
+                <div class="invoice-date-container">
+                    <p>Invoice Date: ${ this.invoiceDateString }</p>
+                </div>
+    
+                <ul class="invoice-heading-data-list">
+                    <li><strong>${ this.invoiceData.fullName }</strong></li>
+                    ${ this.invoiceData.headingData.map(data => (
+                        `<li>${ data.label }: ${ data.value }</li>`
                     )).join("") }
-                    <tr>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-
-                <tfoot>
-                    <tr>
-                        <td>Total</td>
-                        <td>${ invoiceData.coinToUse } ${ totalInvoice }</td>
-                    </tr>
-                </tfoot>
-            </table>
-            ${ TermsAndCondtionsComponent( totalInvoice, invoiceData.termsAndConditionsData ) }
-        </div>
-    </body>
-    </html>
-    `
+                </ul>
+    
+                <table class="invoice-items-table">
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                        ${ this.invoiceData.itemsToCharge.map(item => (
+                            `<tr>
+                                <td>${ item.description }</td>
+                                <td>${ this.invoiceData.coinToUse } ${ item.total }</td>
+                            </tr>`
+                        )).join("") }
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+    
+                    <tfoot>
+                        <tr>
+                            <td>Total</td>
+                            <td>${ this.invoiceData.coinToUse } ${ this.totalInvoice }</td>
+                        </tr>
+                    </tfoot>
+                </table>
+                ${ this.TermsAndCondtionsComponent() }
+            </div>
+        </body>
+        </html>
+        `
+    }
 }
 
-const report = FullReport()
-module.exports = report
+module.exports = new PDFReport()
